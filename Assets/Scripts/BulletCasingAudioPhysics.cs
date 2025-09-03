dc14c35d70e7db9c32e415bba8bd1e6ad0d091a8
Gabriele Lopez <gabriele.ope@gmail.com>
2025-09-03 22:46:02 +0200

    Destroy bullets and casings after set time

--- Assets/Scripts/BulletCasingAudioPhysics.cs
+++ Assets/Scripts/BulletCasingAudioPhysics.cs
@@ -6,11 +6,13 @@ public class BulletCasingAudioPhysics : MonoBehaviour
     public float disableTimer = 5f;
     public AudioClip[] audioClips;
     private AudioSource _audioSource;
+    private float _destroyTimer;
     private Rigidbody _rigidbody;
     private float _timer;
 
     private void Awake()
     {
+        _destroyTimer = disableTimer * 2;
         _audioSource = GetComponent<AudioSource>();
         _audioSource.clip = audioClips[0];
         _rigidbody = GetComponent<Rigidbody>();
@@ -20,6 +22,7 @@ public class BulletCasingAudioPhysics : MonoBehaviour
     {
         _timer += Time.deltaTime;
         if (_timer > disableTimer) _rigidbody.isKinematic = true;
+        if (_timer > _destroyTimer) Destroy(gameObject);
     }
 
     private void OnCollisionEnter(Collision collision)
